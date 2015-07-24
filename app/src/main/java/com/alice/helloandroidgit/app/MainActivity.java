36110a35e8979d28bf4e7417486a581fb9be22cf
AJimmy <1449292153@qq.com>
2015-07-24 15:33:48 +0800

    修改应用首页布局，activity_main布局为LinearLayout

--- app/src/main/java/com/alice/helloandroidgit/app/MainActivity.java
+++ app/src/main/java/com/alice/helloandroidgit/app/MainActivity.java
@@ -11,4 +11,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //this is a comment
+
+    //this is a comment
 }
